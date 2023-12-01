fb0fa8dad9e9eff5491fa729e58aea22d27b7bc2
wangjunhao <2283808819@qq.com>
2023-12-02 00:16:02 +0800

    第一天

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+# 导入 Flask 对象
 from flask import Flask
 
+# 使用Flask对象创建一个app对象
 app = Flask(__name__)
 
 
-@app.route('/')
+# 路由
+@app.route('/')  # “/” 访问的路径  “@” 生成器
 def hello_world():  # put application's code here
     return 'HBNU'
+
+
+@app.route('/login')
+def login():
+    # 登录的功能
+    return '需要实现登录的逻辑'
 
 
 if __name__ == '__main__':
