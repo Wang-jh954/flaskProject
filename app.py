317b8cca81942311ae041a7f89925372792badc6
jh <2283808819@qq.com>
2023-12-02 00:37:41 +0800

    登录界面

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # 导入 Flask 对象
-from flask import Flask
+from flask import Flask,render_template
 
 # 使用Flask对象创建一个app对象
 app = Flask(__name__)
@@ -14,7 +14,11 @@ def hello_world():  # put application's code here
 @app.route('/login')
 def login():
     # 登录的功能
-    return '需要实现登录的逻辑'
+    return render_template('login.html')
+
+
+# 实现其他功能怎么做(例如退出、查看学生信息）等等的其他功能、该怎么实现
+# @app.route('/login_out')
 
 
 if __name__ == '__main__':
